c34f57c3776b99ae9d5d71a57603e5ea2887d306
Alvin Reyvaldo <alvinreyvaldo1@gmail.com>
2025-11-26 14:06:39 +0700

    feat/ init project

---
+++ SplitBill/SplitBill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplitBill
+//
+//  Created by Alvin Reyvaldo on 26/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplitBill/SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by Alvin Reyvaldo on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
